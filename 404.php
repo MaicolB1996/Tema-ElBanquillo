d92e6e4e07821a2663b7c46139691fcc3342a00a
cristian1913 <echeverricristian@gmail.com>
2021-05-12 19:18:14 -0500

    Página error 404 con su contenido y botón hacia el home.
    Cargando contenido de el banquillo

--- 404.php
+++ 404.php
@@ -1,1 +1,10 @@
-ERROR CUCHO
+<?php
+get_header();
+ ?>
+
+ <div class="mensaje_error">
+   <h1>Lo sentimos, la página que estás buscando no se encuentra disponible</h1>
+   <a href="<?php echo get_home_url(); ?>" class="boton_inicio">Regresar al inicio</a>
+ </div>
+
+ <?php get_footer(); ?>
