4f17d5bd3b0c73a324c6670f8be8fdba7d8f630a
cristian1913 <echeverricristian@gmail.com>
2021-05-12 20:12:29 -0500

    carga página de error

--- 404.php
+++ 404.php
@@ -1,10 +1,10 @@
 <?php
 get_header();
  ?>
-
+<!-- Inicio vista -->
  <div class="mensaje_error">
    <h1>Lo sentimos, la página que estás buscando no se encuentra disponible</h1>
    <a href="<?php echo get_home_url(); ?>" class="boton_inicio">Regresar al inicio</a>
  </div>
-
+<!-- Final vista -->
  <?php get_footer(); ?>
